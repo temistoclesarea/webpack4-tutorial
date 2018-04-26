f97659e80c7a249953179f0de643adcd6c389b32
Temístocles Arêa <temistocles.area.br@gmail.com>
2018-04-26 08:22:25 -0300

    hot module replacement study

---
+++ tutorial02/dist/app.d99025e71eca7396d5c9.hot-update.js
@@ -0,0 +1,20 @@
+webpackHotUpdate("app",{
+
+/***/ "./src/print.js":
+/*!**********************!*\
+  !*** ./src/print.js ***!
+  \**********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return printMe; });
+function printMe() {
+    console.log('Updating print.js...');
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvcHJpbnQuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQUE7QUFDQTtBQUNBLEMiLCJmaWxlIjoiYXBwLmQ5OTAyNWU3MWVjYTczOTZkNWM5LmhvdC11cGRhdGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBwcmludE1lKCkge1xyXG4gICAgY29uc29sZS5sb2coJ1VwZGF0aW5nIHByaW50LmpzLi4uJyk7XHJcbn0iXSwic291cmNlUm9vdCI6IiJ9
--- tutorial02/webpack.config.js
+++ tutorial02/webpack.config.js
@@ -1,20 +1,30 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        app: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        port: 8000,
+        hot: true
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(['dist/*.*']),
         new HtmlWebpackPlugin({
-            title: 'Output Management'
-        })
+            title: 'Hot Module Replacement'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    mode: 'development'
 };
